f01894a58d6b16b7f4a12a29e0618856833a0727
SvdNeo <sanjay.d@neoito.com>
2023-12-12 15:39:08 +0530

    task to get a valid number executed in both prompt way and input type method

--- numbers.js
+++ numbers.js
@@ -14,8 +14,50 @@ console.log( Number.isNaN("str") )
 
 //task1
 
-let a = +prompt("Enter the first number")
-let b = +prompt("Enter the second number")
-let c = a+b
-alert(c) 
+// let a = +prompt("Enter the first number")
+// let b = +prompt("Enter the second number")
+// let c = a+b
+//alert(c) 
 
+//task 2
+console.log(+6.35.toFixed(1))// here we get 6.3 as toFixed method rounds of 0.05 to 0 rather than 0.1
+console.log(Math.round(6.35*10)/10)// here we get 6.4 (6.35*10 =63.5) math.round of 63.5 is 64 and 64/10 is 6.4
+
+
+//function to read a number
+function readNumber() {
+    let num;
+    do {
+        num = prompt("Enter a number");
+    } while (isNaN(num) || num === "");
+
+    if (num === null) {
+        return null;
+    } else {
+        return +num;
+    }
+}
+
+alert(readNumber());
+
+
+function readNumber1() {
+    let numInput = document.getElementById("numberInput").value;
+
+    // Check if the entered value is not empty and is a finite number
+    if (numInput !== "" && isFinite(numInput)) {
+      return +numInput; // Convert the input to a number and return
+    } else {
+      return null; // Return null for invalid input
+    }
+  }
+
+  function handleInput() {
+    let result = readNumber1();
+
+    if (result !== null) {
+      alert("You entered a valid number: " + result);
+    } else {
+      alert("Invalid input. Please enter a valid number.");
+    }
+  }
