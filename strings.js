a75dfc63b9ed5c6c2e3581abeed90d4ea48bdfe3
SvdNeo <sanjay.d@neoito.com>
2023-12-12 19:00:55 +0530

    string methods to acces ,getting length,indexOf,slice

---
+++ strings.js
@@ -0,0 +1,14 @@
+let str = "Hello";
+console.log(str.length);// getting length
+console.log(str[-1]);// accessing the strings
+console.log(str.at(-5));
+console.log(str[0]);
+
+let str1 = 'Widget with id';
+console.log(str1.indexOf("with"))
+console.log(str1.indexOf("Widget"))
+console.log(str1.indexOf("id",1))
+
+let str2 = "Competition";
+console.log(str2.slice(2,5));
+console.log(str2.slice(-5,-1));
