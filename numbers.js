2bc3760c89f3606cb82a8c6c1dec022ac7478e53
SvdNeo <sanjay.d@neoito.com>
2023-12-12 17:27:09 +0530

    rendom integer generation

--- numbers.js
+++ numbers.js
@@ -1,70 +1,77 @@
-console.log(123456..toString(36));// using (..) to apply a method onto a number here second dot implies to method
+// console.log(123456..toString(36));// using (..) to apply a method onto a number here second dot implies to method
 
-console.log((123456).toString(36));
+// console.log((123456).toString(36));
 
-console.log(Math.ceil(3.4));// it rounds of to upper limit
-console.log(Math.floor(3.4));// it rounds of to lower limit
-console.log(Math.round(3.4));// it returns logical output
-let round  = 3.145;
-console.log(+round.toFixed(2));// toFixed method returns a string hence it can be converted to number by using unary operator(+)
-console.log( isNaN(NaN) ); // true
-console.log( isNaN("str") );//true
-console.log(NaN === NaN);//false
-console.log( Number.isNaN("str") )
+// console.log(Math.ceil(3.4));// it rounds of to upper limit
+// console.log(Math.floor(3.4));// it rounds of to lower limit
+// console.log(Math.round(3.4));// it returns logical output
+// let round  = 3.145;
+// console.log(+round.toFixed(2));// toFixed method returns a string hence it can be converted to number by using unary operator(+)
+// console.log( isNaN(NaN) ); // true
+// console.log( isNaN("str") );//true
+// console.log(NaN === NaN);//false
+// console.log( Number.isNaN("str") )
 
-//task1
+// //task1
 
-// let a = +prompt("Enter the first number")
-// let b = +prompt("Enter the second number")
-// let c = a+b
-//alert(c) 
+// // let a = +prompt("Enter the first number")
+// // let b = +prompt("Enter the second number")
+// // let c = a+b
+// //alert(c) 
 
-//task 2
-console.log(+6.35.toFixed(1))// here we get 6.3 as toFixed method rounds of 0.05 to 0 rather than 0.1
-console.log(Math.round(6.35*10)/10)// here we get 6.4 (6.35*10 =63.5) math.round of 63.5 is 64 and 64/10 is 6.4
-
-
-//function to read a number
-function readNumber() {
-    let num;
-    do {
-        num = prompt("Enter a number");
-    } while (isNaN(num) || num === "");
-
-    if (num === null) {
-        return null;
-    } else {
-        return +num;
-    }
-}
-
-alert(readNumber());
+// //task 2
+// console.log(+6.35.toFixed(1))// here we get 6.3 as toFixed method rounds of 0.05 to 0 rather than 0.1
+// console.log(Math.round(6.35*10)/10)// here we get 6.4 (6.35*10 =63.5) math.round of 63.5 is 64 and 64/10 is 6.4
 
 
-function readNumber1() {
-    let numInput = document.getElementById("numberInput").value;
+// //function to read a number
+// function readNumber() {
+//     let num;
+//     do {
+//         num = prompt("Enter a number");
+//     } while (isNaN(num) || num === "");
 
-    // Check if the entered value is not empty and is a finite number
-    if (numInput !== "" && isFinite(numInput)) {
-      return +numInput; // Convert the input to a number and return
-    } else {
-      return null; // Return null for invalid input
-    }
-  }
+//     if (num === null) {
+//         return null;
+//     } else {
+//         return +num;
+//     }
+// }
 
-  function handleInput() {
-    let result = readNumber1();
+// alert(readNumber());
 
-    if (result !== null) {
-      alert("You entered a valid number: " + result);
-    } else {
-      alert("Invalid input. Please enter a valid number.");
-    }
-  }
+
+// function readNumber1() {
+//     let numInput = document.getElementById("numberInput").value;
+
+//     // Check if the entered value is not empty and is a finite number
+//     if (numInput !== "" && isFinite(numInput)) {
+//       return +numInput; // Convert the input to a number and return
+//     } else {
+//       return null; // Return null for invalid input
+//     }
+//   }
+
+//   function handleInput() {
+//     let result = readNumber1();
+
+//     if (result !== null) {
+//       alert("You entered a valid number: " + result);
+//     } else {
+//       alert("Invalid input. Please enter a valid number.");
+//     }
+//   }
 
   //Write the function random(min, max) to generate a random floating-point number from min to max (not including max).
   function random(min,max){
-let h = Math.random*(max-min)+min
+let h = Math.random()*(max-min)+min
 return h
   }
   random(1,5)
+
+  //Create a function randomInteger(min, max) that generates a random integer number from min to max including both min and max as possible values.
+  function randomInt(min,max){
+    let h = Math.floor(Math.random()*(max-min+1)+min)
+    return h
+      }
+      console.log(randomInt(1,5))
