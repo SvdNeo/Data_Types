ae7fff65facc191440aff04baf192835261b848d
SvdNeo <sanjay.d@neoito.com>
2023-12-13 21:19:23 +0530

    filter range

--- arrayMethods.js
+++ arrayMethods.js
@@ -3,3 +3,12 @@ function camelize(str){
     return str.split("-").map((letter,index)=> index===0 ? letter : letter[0].toUpperCase()+letter.slice(1)).join("")
 }
 console.log(camelize("list-style-image"))
+
+
+//filter range 
+let arr = [5, 3, 8, 1];
+
+function filterRange(arr, a, b){
+    return arr.filter(i=>(i>=a && i<=b))
+}
+console.log(filterRange(arr,1,4))
