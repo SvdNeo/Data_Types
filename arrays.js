d6788107047386f02a1f8d63f1297fa676e412cb
SvdNeo <sanjay.d@neoito.com>
2023-12-13 11:25:00 +0530

    accessing mixed arrays

--- arrays.js
+++ arrays.js
@@ -5,3 +5,10 @@ console.log(fruits);
 fruits[3] = "Water-Melon";// adding the elements
 console.log(fruits);
 console.log(fruits.length);
+
+let arr = ["Steeve",{class:"Sixth"},[4,5,6],true,9,null,function() { console.log('Hello!'); }]; // mixed array
+console.log(arr[1].class);
+console.log(arr[2][0]);
+console.log(arr[3]);
+console.log(typeof arr[5]);
+arr[6]()
