8b7667db653ae0c626cd0cbb1d9bd24bfafb06a3
SvdNeo <sanjay.d@neoito.com>
2023-12-13 11:04:01 +0530

    accessing,adding,replacing and getting the length of arrays

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let fruits = ["apple","grapes","guava"];
+console.log(fruits[0]);// accessing the arrays
+fruits[2] = "Orange";// replacing the elements
+console.log(fruits);
+fruits[3] = "Water-Melon";// adding the elements
+console.log(fruits);
+console.log(fruits.length);
