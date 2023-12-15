bf2fede2aeafa8155b58ce22b242924d6e8ba220
SvdNeo <sanjay.d@neoito.com>
2023-12-15 09:55:18 +0530

    setting multiple entries and different data types as keys

--- map_set.js
+++ map_set.js
@@ -2,5 +2,7 @@ let map = new Map();
 map.set("1","str1");
 map.set(1,"num");
 map.set(true,"bol1")
+map.set({name:"sanjay",id:"1"},"nameObj").set([2,3,4,5,"fruits"],{numbers:"continuous"})
+console.log(map)
 console.log(map.get(1))
 console.log(map.get("1"))
