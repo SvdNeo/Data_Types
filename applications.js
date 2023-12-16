c78534e96f87c50e5f35a766beb36219aa096684
SvdNeo <sanjay.d@neoito.com>
2023-12-17 00:28:22 +0530

    traversing through array of objects

--- applications.js
+++ applications.js
@@ -15,5 +15,24 @@ const products = [
     { id: 102, name: 'Smartphone', price: 500 },
     { id: 103, name: 'Headphones', price: 80 }
   ];
-  const pdtName = products.map(i=> i.price).reduce((acc,cur)=> {return acc+cur},0)
-  console.log(pdtName);
+  const totalPdtPrice = products.map(i=> i.price).reduce((acc,cur)=> {return acc+cur},0)// other option is products.reduce((sum,pdt)=>sum+pdt.price,0)
+  const filtered = products.filter(i=> i.price>=500).map(x=>x.name)
+  console.log(totalPdtPrice);
+  console.log(filtered)
+
+  const voters = [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 32 },
+    { id: 3, name: 'Charlie', age: 22 },
+    { id: 4, name: 'David', age: 19 },
+    { id: 5, name: 'Eve', age: 28 }
+  ];
+  const notEligible = voters.some(i=>i.age<18)
+  console.log(notEligible)
+  const eligible = voters.every(i=>i.age>=18)
+  console.log(eligible)
+
+  const firstUserKeys = Object.values(voters[0]);
+console.log(firstUserKeys);
+const array = voters.map(i=>[i.id,i.name,i.age]);
+console.log(array)
