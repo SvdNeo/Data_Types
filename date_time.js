4b1f9759fde1c7c76ea7e248a68e5357d9274cbd
SvdNeo <sanjay.d@neoito.com>
2023-12-18 20:41:26 +0530

    date and time methods

---
+++ date_time.js
@@ -0,0 +1,15 @@
+let now = new Date() 
+console.log(now)
+let Jan02_1970 = new Date(-24 * 3600 * 1000);
+console.log(Jan02_1970)
+let Jan01_1970 = new Date(24 * 3600 * 1000);
+console.log(Jan01_1970)
+let Jan03_1970 = new Date(0);
+console.log(Jan03_1970)
+let date = new Date();
+console.log(date.getUTCHours())
+console.log(date.getTime())
+let date1 = new Date(2016, 2, 28);
+//date1.setDate(date.getDate() + 2);
+date1.setSeconds(date1.getSeconds() + 70);
+console.log( date1 );
