739c95f0443c4c386b533d6512cdd1177e6b2a13
SvdNeo <sanjay.d@neoito.com>
2023-12-19 11:47:39 +0530

    function,undefined,symbol is ignored in json

--- json.js
+++ json.js
@@ -8,3 +8,18 @@ let student = {
   };
   let json = JSON.stringify(student);
   console.log(typeof json)
+  console.log(JSON.stringify(undefined))
+  console.log(JSON.stringify([1,'a',3]))
+
+  let meetup = {
+    title: "Conference",
+    room: {
+      number: 23,
+      participants: ["john", "ann"]
+    },
+    user:function(a,b){return a+b},
+    user2:undefined,
+    [Symbol('id')] : 123
+
+  };
+  console.log(JSON.stringify(meetup))
